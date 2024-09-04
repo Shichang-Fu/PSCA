be107f6bb3937e5600347ee720dc93e38f68be05
Shichang-Fu <103252169+Shichang-Fu@users.noreply.github.com>
2024-09-04 16:22:32 +0800

    Update movetxtwithimages.py

--- dataset/utils/movetxtwithimages.py
+++ dataset/utils/movetxtwithimages.py
@@ -1,26 +1,20 @@
 import os
 import shutil
 
-# 文件夹路径
-all_labels_folder = "F:\\fsc_dataset\\UAV-DT\\dataset\\txt"
-selected_images_folder = "F:\\fsc_dataset\\UAV-DT\\dataset\\weather\\val\\images\\daylight"
-selected_labels_folder = "F:\\fsc_dataset\\UAV-DT\\dataset\\weather\\val\\txt\\daylight"
+all_labels_folder = "UAV-DT\\dataset\\txt"
+selected_images_folder = "UAV-DT\\dataset\\weather\\val\\images\\daylight"
+selected_labels_folder = "UAV-DT\\dataset\\weather\\val\\txt\\daylight"
 
-# 遍历新的图片文件夹
+
 for image_filename in os.listdir(selected_images_folder):
-    # 构建图片文件路径
-    image_path = os.path.join(selected_images_folder, image_filename)
 
-    # 构建标签文件路径
-    label_filename = image_filename.replace(".jpg", ".txt")  # 假设标签文件是以.txt结尾的
+    image_path = os.path.join(selected_images_folder, image_filename)
+    label_filename = image_filename.replace(".jpg", ".txt")
     label_path = os.path.join(all_labels_folder, label_filename)
 
-    # 如果标签文件存在，则进行复制
-    if os.path.exists(label_path):
-        # 构建目标标签文件夹路径
-        destination_label_path = os.path.join(selected_labels_folder, label_filename)
 
-        # 复制标签文件到新的标签文件夹
+    if os.path.exists(label_path):
+        destination_label_path = os.path.join(selected_labels_folder, label_filename)
         shutil.copy(label_path, destination_label_path)
     else:
         print(f"Warning: No corresponding label file found for image {image_filename}. Skipping.")
